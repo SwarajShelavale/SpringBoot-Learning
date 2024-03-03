ea5d1a90b1e32156aa10a1d9388f3ba9327ec3b4
Swaraj Shelavale <shelavaleswaraj1610@gmail.com>
2024-03-04 03:17:10 +0530

    First URL based API created

--- src/main/java/com/swaraj/firstdemospringproject/MyFirstAPI.java
+++ src/main/java/com/swaraj/firstdemospringproject/MyFirstAPI.java
@@ -2,6 +2,7 @@ package com.swaraj.firstdemospringproject;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +15,16 @@ public class MyFirstAPI {
 
     @RequestMapping("/hello/{nameOfPerson}")
     public String sayHelloToPerson(@PathVariable("nameOfPerson") String nameOfPerson){
-        return "Welcome to SpringBoot API - " + nameOfPerson;
+        return "Welcome to SpringBoot API:-" + nameOfPerson;
     }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String createNewProduct() {
+        return "new Product created";
+     }
+
+
+
+
+
 }
